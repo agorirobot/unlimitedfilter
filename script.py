7ad0f1569146cec5d1533a89005dd2481c928b3a
Personal Robot <88285322+agorirobot@users.noreply.github.com>
2021-08-19 15:57:48 +0530

    Update and rename script.py to script.py

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ class Script(object):
 
     START_MSG = """<b>Hy {},
 
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
+<code>I'm an advanced filter bot with many capabilities!
+There is no practical limits for my filtering capacity :)</code>
 
 See <i>/help</i> for commands and more details.</b>
 """
